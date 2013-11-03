e29077efed42141cf6dd3029d326001b0742a573
Henk Jan Wils <henkjan@paypro.nl>
2013-11-03 22:39:44 +0100

    Finish signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new( user_params )
     if @user.save
+      sign_in @user
       flash[:success] = "We are happy to welcome you at Coursegenerator!"
       redirect_to @user
     else
