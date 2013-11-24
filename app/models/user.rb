9a3b350d14cedcb0a388f02bc901da0ab827b525
Henk Jan Wils <henkjan@paypro.nl>
2013-11-24 22:10:26 +0100

    Add user micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+
   validates :name, presence: true, length: { maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
@@ -7,6 +9,11 @@ class User < ActiveRecord::Base
 
   before_save { email.downcase! }
   before_create :create_remember_token
+
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
 
   factory :user do
-    sequence(:name)  { |n| "#{Faker::Name.name} #{n}" }
+
+    sequence(:name)  { |n| "Person #{n}" }
     sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
@@ -11,4 +12,10 @@ FactoryGirl.define do
     end
 
   end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
+
 end
